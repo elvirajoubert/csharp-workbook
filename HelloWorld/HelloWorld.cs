dca85035f85bf5d89bfd2cd770b06740b24827ef
Elvira Joubert <reagent01@gmail.com>
2019-01-07 20:43:22 -0600

    helloworld initial commit

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Me!");
+            string name = "";
+		int age = 0;
+		int year = 0;
+		
+		Console.WriteLine("Please enter your name: ");
+		name = Console.ReadLine();
+		Console.WriteLine("Please enter your age: ");
+		age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter the year: ");
+		year = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Hello! My name is {0} and I am {1} years old. I was born in {2}.", name, age, year-age);
         }
     }
 }
