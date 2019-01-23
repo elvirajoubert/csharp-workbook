49fbc866e5323260b7cde2d5da8aa0c40bf66765
Elvira Joubert <reagent01@gmail.com>
2019-01-23 15:02:47 -0600

    final commit

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -4,45 +4,5 @@ namespace PigLatin
 {
     class Program
     {
-        private static string restWord;
-
-        static void Main(string[] args)
-        {
-TranslateWord("World");
-        }
-          public static string TranslateWord(string word)
-        {
-            // your code goes here
-            // return word;
-        // {
-            // your code goes here
-
-//Alphabet code
-
-// string alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-// // `.Substring` takes a starting index and the length of what 
-// // you want to include to the right
-// string firstThird = alphabet.Substring(0, 8);
-// string secondThird = alphabet.Substring(8, 8);
-// string anotherThird = alphabet.Substring(17);
-
-// System.Console.WriteLine(firstThird);
-// System.Console.WriteLine(secondThird);
-// System.Console.WriteLine(anotherThird);
-// return word;
-//         }
-//     }
-// }
-
-
- //"car" => "arcay" ---->Task
-string s5 = "car";
-string word Console.ReadLine(;)
-restWord = s5.Substring(3);
-firstLetter = s5.Substring(0);
-
-Console.WriteLine(restWord+"ay"+ firstLetter +"ay");
-
-    return word;
     }
+}
--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -12,55 +12,54 @@ namespace RockPaperScissors
             Console.WriteLine("Enter hand 2:");
             string hand2 = Console.ReadLine().ToLower();
             Console.WriteLine(hand2);
-            
+
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-        
-         public static string CompareHands(string hand1, string hand2)
-    {
-        //check for tie
-        if (hand1 == hand2)
-        {
-            return "It's a tie";
-        }
 
-        if(hand1 == "rock")
+        public static string CompareHands(string hand1, string hand2)
         {
-            if(hand2 == "paper")
+            //check for tie
+            if (hand1 == hand2)
             {
-                return "Hand2 Won!";
-
+                return "It's a tie";
             }
 
-            return "Hand1 Won1";
-        }
-        
-            if(hand1 == "paper")
-            {
-            if(hand2 == "scissors")
+            if (hand1 == "rock" && hand2 == "paper")
             {
                 return "Hand2 Won!";
             }
 
-            return "Hand1 Won!";
-            }
-        
-        
-        if (hand1 == "scissors")
-        {
-            if (hand2 == "rock")
+            else if (hand1 == "paper" && hand2 == "scissors")
             {
                 return "Hand2 Won!";
             }
+            else if (hand1 == "scissors" && hand2 == "rock")
+            {
+                return "Hand2 Won!";
+            }
+            else if (hand1 == "paper" && hand2 == "rock")
+            {
+                return "Hand1 Won!";
+            }
+            //fixlogicfor hand1 win
+            else if (hand1 == "rock" && hand2 == "scissors")
+            {
+                return "Hand1 Won!";
+            }
+            else if (hand1 == "scissors" && hand2 == "paper")
+            {
+                return "Hand1 Won!";
+            }
+            else
+            {
+                return "Unknow winner";
+            }
 
-            return "Hand1 Won!";
-        
         }
-    }    
-}
 
+    }
 }
 
