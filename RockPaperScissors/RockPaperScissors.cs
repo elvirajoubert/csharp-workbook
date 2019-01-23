95c2e6c909b72adb7c05d4b9bd49de2e9db1956b
Elvira Joubert <reagent01@gmail.com>
2019-01-23 10:41:24 -0600

    checking errors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -10,22 +10,55 @@ namespace RockPaperScissors
             string hand1 = Console.ReadLine().ToLower();
             Console.WriteLine(hand1);
             Console.WriteLine("Enter hand 2:");
-            Console.WriteLine(hand2);
             string hand2 = Console.ReadLine().ToLower();
+            Console.WriteLine(hand2);
+            
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
         
-        public static string CompareHands(string hand1, string hand2)
+         public static string CompareHands(string hand1, string hand2)
+    {
+        //check for tie
+        if (hand1 == hand2)
         {
-            // Your code here
-            return hand1 + ' ' + hand2;
-
-            if (hand1 == "rock" && "paper")
-                
-
+            return "It's a tie";
         }
-    }
+
+        if(hand1 == "rock")
+        {
+            if(hand2 == "paper")
+            {
+                return "Hand2 Won!";
+
+            }
+
+            return "Hand1 Won1";
+        }
+        
+            if(hand1 == "paper")
+            {
+            if(hand2 == "scissors")
+            {
+                return "Hand2 Won!";
+            }
+
+            return "Hand1 Won!";
+            }
+        
+        
+        if (hand1 == "scissors")
+        {
+            if (hand2 == "rock")
+            {
+                return "Hand2 Won!";
+            }
+
+            return "Hand1 Won!";
+        
+        }
+    }    
 }
+
