559253c8608d85b0f1d2fb1a776bc5b748b7fad8
Elvira Joubert <reagent01@gmail.com>
2019-01-23 20:04:50 -0600

    initial commit

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,9 +4,49 @@ namespace FizzBuzz
 {
     class Program
     {
+        /*
+          FizzBuzz:
+          Count from the number 1
+          Any number divisible by 3 = Fizz
+          Any number divisible by 5 = Buzz
+          Numbers that are divisible by both = FizzBuzz
+           */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("FizzBuzz!");
+            Console.WriteLine("Please, Enter a Number...");
+            int numEntered = Convert.ToInt32(Console.ReadLine());
+            fizzBuzz(numEntered);
+
+
+        }
+
+        public static void fizzBuzz(int numEntered)
+        {
+            for (int i = numEntered; i < 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("buzz");
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
+
+
+
+
