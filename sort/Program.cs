579a76a28b52cdc4ed12c78d75c524a0b5f97d60
Elvira Joubert <reagent01@gmail.com>
2019-01-27 18:51:16 -0600

    initial commit

---
+++ sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] x = new int[] { 14, 4, 17, 5, 8, 2, 1, 3, 9 };
+            int temp = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = i + 1; j < x.Length; j++)
+                {
+                    if (x[i] > x[j])
+                    {
+                        temp = x[j];
+                        x[j] = x[i];
+                        x[i] = temp;
+                    }
+                }
+                Console.WriteLine(x[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
