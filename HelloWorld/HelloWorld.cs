53dfc0ff026fac6244d5d8f8fb81cce4ef2ac174
Elvira Joubert <reagent01@gmail.com>
2019-01-28 11:41:50 -0600

    deleted unneccesary folder to make the program run correctly

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,21 +4,21 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Program.Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello Me Elvira!");
+            Console.WriteLine("Hello User!");
             string name = "";
-		int age = 0;
-		int year = 0;
-		
-		Console.WriteLine("Please enter your name: ");
-		name = Console.ReadLine();
-		Console.WriteLine("Please enter your age: ");
-		age = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine("Please enter the year: ");
-		year = Convert.ToInt32(Console.ReadLine());
-		
-		Console.WriteLine("Hello! My name is {0} and I am {1} years old. I was born in {2}.", name, age, year-age);
+            int age = 0;
+            int year = 0;
+
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the current year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello! Your name is {0} and You are {1} years old. You were born in {2}.", name, age, year - age);
         }
     }
 }
