abfe601cea6684579c9f4911c0e5f5bbaed288fa
Elvira Joubert <reagent01@gmail.com>
2019-01-28 20:47:22 -0600

    initial commit

---
+++ candles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace candles
+{
+public class Program
+{
+	
+	public static  void Main(string[]args)
+	{
+        int greatest = 0;
+	    int count = 0;
+		int[]allCandles = new int[]{2, 5, 3, 1 };
+		int count = BirthdayCakeCounter(allCandles.Length, allCandles);
+		Console.WriteLine("I can blow out {count} candles");
+	}
+	public static BirthdayCakeCounter(int length, int[] candles)
+		{
+			foreach(var candle in candles)
+			{
+			if (candle > greatest)
+			{
+				greatest = candles;
+				count = 0;
+			}
+				if (candle == greatest)
+				{
+					count++;
+				}
+			}
+		return count;
+	}
+}
+}
