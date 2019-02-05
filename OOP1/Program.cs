e4860ca5e0f19942854a81b2cceefe76b3d4bcac
Elvira Joubert <reagent01@gmail.com>
2019-02-04 20:23:55 -0600

    finalized and removed nested classes

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,72 +1,101 @@
 ﻿using System;
-					
-public class Program
-{
-	public static void Main()
-	{   
-        var person = new Person();
-        Car blueCar = new Car("blue");
-		Garage smallGarage = new Garage(2);
-		
-        car.addPerson(firstName, lastName);
-		smallGarage.ParkCar(blueCar, 0);
-        Console.WriteLine(Car.Person);
-		Console.WriteLine(smallGarage.Cars);
-	}
-}
+using System.Collections.Generic;
 
-class Car
+namespace OOP1
 {
-    private Person[] passengers;
-    public Car(string initialColor)
+    public class Program
     {
-    	Color = initialColor;
-        this.passengers = new Person[];
-    }
-    
-    public string Color { get; private set; }
-}
-class Person
-{
-    private string firstName;
-	private string lastName;
-	public Person(string firstName, string lastName)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    class Garage
-{
-    private Car[] cars;
-    
-    public Garage(int initialSize)
-    {
-    	Size = initialSize;
-	    this.cars = new Car[initialSize];
-    }
-    
-    public int Size { get; private set; }
-    
-    public void ParkCar (Car car, int spot)
-    {
-        cars[spot] = car;
-    }
-    
-    public string Cars {
-		get {
-			for (int i = 0; i < cars.Length; i++)
-			{
-				if (cars[i] != null) {
-					Console.WriteLine(String.Format("The {0} car is in spot {1} and passenger is .",Car.Person, cars[i].Color, i));
-				}
-			}
-            
-			return "That's all!";
-		
+        public static void Main()
+        {
+            Garage smallGarage = new Garage(2);
+            Car blueCar = new Car("blue");
+            Person autobot = new Person("Autobot");
+            Person bumblebee = new Person("BumbleBee");
+            blueCar.addPerson(autobot, 0);
+            blueCar.addPerson(bumblebee, 1);
+            smallGarage.ParkCar(blueCar, 0);
+            Console.WriteLine(smallGarage.Cars);
         }
-	
+    }
+
+    public class Car
+    {
+        public Car(string initialColor)
+        {
+            Color = initialColor;
+            this.passengers = new List<Person>();
+
+        }
+
+
+        public string Color { get; private set; }
+
+        public List<Person> passengers;
+
+        public void addPerson(Person passenger, int spot)
+        {
+            passengers.Add(passenger);
+
+        }
+
+        public string Passengers
+        {
+            get
+            {
+                string passengerNames = "";
+                for (int i = 0; i < passengers.Count; i++)
+                {
+                    if (passengers[i] != null)
+                    {
+                        passengerNames += passengers[i].Name + " ";
+                    }
+                }
+                return passengerNames;
+            }
+        }
+    }
+    public class Garage
+    {
+        private Car[] cars;
+
+        public Garage(int initialSize)
+        {
+            Size = initialSize;
+            this.cars = new Car[initialSize];
+        }
+        public int Size { get; private set; }
+
+        public void ParkCar(Car car, int spot)
+        {
+            cars[spot] = car;
+        }
+
+        public string Cars
+        {
+            get
+            {
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    if (cars[i] != null)
+                    {
+                        Console.WriteLine(String.Format("The {0} car is in spot {1} and the people in that car are: {2}", cars[i].Color, i, cars[i].Passengers));
+                    }
+                }
+                return "That's all!";
+
+            }
+        }
+    }
+
+    public class Person
+    {
+        public Person(string initialName)
+        {
+            Name = initialName;
+        }
+        public string Name { get; private set; }
     }
 }
-}
+
+
 
