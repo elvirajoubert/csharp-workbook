fa28e7c42e4e97c0eb30b09c5a786c02d98740ab
Elvira Joubert <reagent01@gmail.com>
2019-02-10 12:16:24 -0600

    fixed roster and Ship constructor

--- StarWars/Program.cs
+++ StarWars/Program.cs
@@ -5,16 +5,16 @@ using System.Linq;
 
 namespace StarWars
 {
-
-
     public class Program
     {
         public static void Main()
         {
+            //variables
             Person leia = new Person("Leia", "Organa", "Rebel");
+            Person hansolo = new Person("Han", "Solo", "Rebel");
             Person darth = new Person("Darth", "Vader", "Imperial");
-            Ship falcon = new Ship("Rebel", "Smuggling", 2);
-            Ship tie = new Ship("Tie", "Fighter", 1);
+            Ship falcon = new Ship("Millennium Falcon", "Rebel", "Smuggling", 2);
+            Ship tie = new Ship("Tie Fighter", "Imperial", "Fighter", 1);
             Ship xwing = new Ship("X Wing", "Rebel", "Fighter", 1);
             Station class1 = new Station("Class 1", "Rebel", 2);
             Station deathStar = new Station("Death Star", "Imperial", 2);
@@ -41,133 +41,123 @@ namespace StarWars
     }
 
 
-//creates a person with a first & last name, their alliance
-public class Person
-{
-    private string firstName;
-    private string lastName;
-    private string alliance;
-    public Person(string firstName, string lastName, string alliance)
+    //creates a person with a first & last name, their alliance
+    public class Person
     {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.alliance = alliance;
-    }
-
-    public string FullName
-    {
-        get
+        private string firstName;
+        private string lastName;
+        private string alliance;
+        public Person(string firstName, string lastName, string alliance)
         {
-            return this.firstName + " " + this.lastName;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.alliance = alliance;
         }
 
-        set
+        public string FullName
         {
-            string[] names = value.Split(' ');
-            this.firstName = names[0];
-            this.lastName = names[1];
-        }
-    }
-}
-
-class Ship
-{
-    public string name;
-    public Person[] passengers;
-    public Ship(string alliance, string type, int size)
-    {
-        this.name = name;
-        this.Type = type;
-        this.Alliance = alliance;
-        this.passengers = new Person[size];
-    }
-
-    public string Type
-    {
-        get;
-        set;
-    }
-
-    public string Alliance
-    {
-        get;
-        set;
-    }
-
-    public string Passengers
-    {
-        get
-        {
-            String formatted = " ";
-            foreach (var person in passengers)
+            get
             {
-                formatted += person + "\n";
-                Console.WriteLine(String.Format("{0}", person.FullName));
+                return this.firstName + " " + this.lastName;
             }
 
-            return "That's Everybody!";
+            set
+            {
+                string[] names = value.Split(' ');
+                this.firstName = names[0];
+                this.lastName = names[1];
+            }
         }
     }
 
-    public void EnterShip(Person person, int seat)
+    class Ship
     {
-        passengers[seat] = person;
+        public string name;
+        public Person[] passengers;
+        public Ship(string name, string alliance, string type, int size)
+        {
+            this.name = name;
+            this.Type = type;
+            this.Alliance = alliance;
+            this.passengers = new Person[size];
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Alliance
+        {
+            get;
+            set;
+        }
+
+        public string Passengers
+        {
+            get
+            {
+                String formatted = " ";
+                foreach (var person in passengers)
+                {
+                    formatted += person + "\n";
+                    Console.WriteLine(String.Format("{0}", person.FullName));
+                }
+
+                return "That's Everybody!";
+            }
+        }
+
+        public void EnterShip(Person person, int seat)
+        {
+            passengers[seat] = person;
+        }
+
+        public void ExitShip(int seat)
+        {
+            this.passengers[seat] = null;
+
+        }
     }
-
-    public void ExitShip(int seat)
+    class Station
     {
-        this.passengers[seat] = null;
+        public string StationName;
+        private Ship[] port;
+        public Station(string StationName, string alliance, int seat)
+        {
+            this.StationName = StationName;
+            this.Alliance = alliance;
+            this.port = new Ship[seat];
+        }
+        public string station
+        {
+            get;
+            set;
+        }
+        public string Alliance
+        {
+            get;
+            set;
+        }
+        public void EnterStation(Ship name, int spot)
+        {
+            this.port[spot] = name;
+        }
+        public void ExitStation(Ship name, int spot)
+        {
+            this.port[spot] = null;
+        }
 
+        public string roster(Ship name)
+        {
+            string rosterNames = "";
+            for (int i = 0; i < name.passengers.Length; i++)
+            {
+                rosterNames += String.Format("Passenger {0} is on Ship {1}.\n", name.passengers[i].FullName, name.name);
+            }
+            return rosterNames;
+        }
     }
 }
-class Station
-{
-    public string StationName;
-    private Ship[] port;
-    public Station(string StationName, string alliance, int seat)
-    {
-        this.StationName = StationName;
-        this.Alliance = alliance;
-        this.port = new Ship[seat];
-    }
-    public string station
-    {
-        get;
-        set;
-    }
-    public string Alliance
-    {
-        get;
-        set;
-    }
-    public void EnterStation(Ship name, int spot)
-    {
-        this.port[spot] = name;
-    }
-    public void ExitStation(Ship name, int spot)
-    {
-        this.port[spot] = null;
-    }
 
-    // public string Ships
-    // {
-    //     Console.WriteLine(String.Format("The {0}, {1} ship arrived at the port {2}.", ships[i].NameShip, ships[i].Type, i));  
-          
-    // }
-    //     return "";
-      
-     
-
-    
-//     public void roster(Ship name)
-//     {
-//     string rosterNames = "";
-//     for (int = 0; i < namespace.passengers.Length; i++)
-//     {
-//     rosterNames += String.Format("Passenger {0} is on Ship {1}.\n, name,passengers[i].FullName, name.name");
-//     }
-//     return rosterNames;
-// }
-
-}
-}
