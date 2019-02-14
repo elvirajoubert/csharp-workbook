441de5925c3a8789b73d320599b84bcebe47913c
Elvira Joubert <reagent01@gmail.com>
2019-02-13 19:43:36 -0600

    created new branch and new dir

---
+++ whiteboardingchallenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace whiteboardingchallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine();
+            StringTransform result;
+            string reversed;
+            result = new StringTransform();
+            reversed = result.Transform("hell");
+            System.Console.WriteLine(reversed);
+
+        }
+    }
+
+    class StringTransform
+    {
+
+        public string Transform(string v)
+        {
+            if (v == null)
+            {
+                throw new ArgumentNullException("v");
+            }
+
+            string result = String.Empty;
+            int i;
+
+            for (i = v.Length-1; i >= 0; i--)
+            {
+                result += v[i];
+            }
+            return result;
+
+        }
+    }
+
+}
+
