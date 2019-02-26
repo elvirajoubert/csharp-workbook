6f1e2458bf1868f9c0e032d1ee7324270639efc4
Elvira Joubert <reagent01@gmail.com>
2019-02-26 10:01:51 -0600

    initial commit

--- Linq/Linq.cs
+++ Linq/Linq.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Linq
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+            
+            students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+            students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+            students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+            students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
+           
+
+            IEnumerable<Student> negativeBalance = from currentStudent in students
+            where currentStudent.Balance < 0
+            select currentStudent;
+
+            Console.WriteLine("All the students with a negative balance");
+            foreach(Student currentStudent in negativeBalance)
+            {
+                Console.WriteLine(currentStudent.Name);
+            }
+
+        }
+    }
+    public class Student 
+    {
+        public string Name {get; set;}
+        public string Phone {get; set;}
+        public string Address {get; set;}
+        public int Balance {get; set;}
+        
+        public Student(string name, string phone, string address , int balance)
+        {
+
+            Name = name;
+            Phone = phone;
+            Address = address;
+            Balance = balance;
         }
     }
 }
---
+++ enums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in Enum.GetValues(typeof(System.DayOfWeek)))
+            {
+                var enumMonth = (DayOfWeek)item;
+                var point = new System.Drawing.Point(0, 0);
+                point = new System.Drawing.Point(5, 5);
+                point.X = point.X * point.X;
+                Console.WriteLine();
+
+                Console.WriteLine(enumMonth.ToString());
+                
+            }
+        
+    
+        }
+
+    public struct StructPoint
+        {
+            public StructPoint(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public int X { get; set; }
+
+            public int Y { get; set; }
+
+            //public object MyObject { get; set; }
+        }
+
+    }
+}
+
