a6f5fa01912ff8ea7f57d0797db9698ba36066da
Elvira Joubert <reagent01@gmail.com>
2019-02-28 08:18:10 -0600

    initial commit

--- Files/Files.cs
+++ Files/Files.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
 
 namespace Files
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void SimpleWay(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string textToWrite = string.Join(" ", args);
+
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string documentName = "Simple Way";
+            string documentExtention = "txt";
+            string fullPathToFile = $"{documentsPath}/{documentName}.{documentExtension}";
         }
+
+        // {
+        //     Output();
+        //     public static void  IEnumerable<string> Input; 
+
+        //     List <String> output;
+
+        //     foreach (string item in input)
+        //     {
+        //         if (item.Length > 3)
+        //         {
+        //         return output.Add(item);
+        //         }
+        //     }
+        //     return output;
+
+        // }
     }
 }
