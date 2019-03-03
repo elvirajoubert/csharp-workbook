85ed2e91b4b38b15f1f90f859f9bb06f0ce3ef7d
Elvira Joubert <reagent01@gmail.com>
2019-03-03 10:22:19 -0600

    initial commit

---
+++ checkpoint2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace checkpoint2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
