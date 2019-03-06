4f72733a3d0e8081795082f6ce75e09fd8405610
Elvira Joubert <reagent01@gmail.com>
2019-03-06 11:14:58 -0600

    added specs

--- Files/Files.cs
+++ Files/Files.cs
@@ -1,35 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Files
+
 {
-    class Program
-    {
-        public static void SimpleWay(string[] args)
-        {
-            string textToWrite = string.Join(" ", args);
+    class Program {
+        static void Main (string[] args) {
 
-            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string documentName = "Simple Way";
-            string documentExtention = "txt";
-            string fullPathToFile = $"{documentsPath}/{documentName}.{documentExtension}";
+            Console.WriteLine ("Hello World");
         }
+        class Files {
+            private string spec1 = "This is a text file.";
+            private string spec2 = "This is a text file and I can edit it.";
+            public string createFile (string myFileName) {
+                string txt = ".txt";
+                string fullFileName = myFileName + txt;
+                string myPath = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
 
-        // {
-        //     Output();
-        //     public static void  IEnumerable<string> Input; 
+                string fullPath = $"{myPath}\\{fullFileName}";
 
-        //     List <String> output;
+                using (FileStream fs = File.Create (fullPath))
+                using (StreamWriter sw = new StreamWriter (fs)) {
+                    sw.WriteLine (spec1);
+                }
+                Console.WriteLine ("Completed");
+                return fullPath;
 
-        //     foreach (string item in input)
-        //     {
-        //         if (item.Length > 3)
-        //         {
-        //         return output.Add(item);
-        //         }
-        //     }
-        //     return output;
+            }
 
-        // }
+            public void removeFile (string fileName) {
+                File.Delete (fileName);
+                Console.WriteLine ("This file has been deleted");
+            }
+
+            public string editFile (string path) {
+                using (FileStream fs1 = File.OpenWrite (path))
+                using (StreamWriter sw1 = new StreamWriter (fs1)) {
+                    sw1.WriteLine (spec2);
+                }
+                Console.WriteLine ("The file at {0} has been edited", path);
+                return path;
+                
+            }
+
+            public void numberOfWords (string filePath) {
+                string[] wordsSentence;
+                wordsSentence = File.ReadAllLines (filePath, Encoding.UTF8);
+                int count = wordsSentence.Length;
+                Console.WriteLine ("Total number of words in this file is.{0}", count);
+            }
+
+            public void longestWord (string filePath) {
+                var file = File.ReadAllLines (filePath, Encoding.UTF8);
+                var wordList = new List<string> (file);
+                string longestWord = "";
+                int longest = 0;
+
+                foreach (string s in wordList) {
+                    if (s.Length > longest) {
+                        longestWord = s;
+                        longest = s.Length;
+                    }
+                }
+                Console.WriteLine ("The longest word is {0}", longestWord);
+
+            }
+
+        }
     }
 }
